09b360fc1e5cab265d812202bf2c95477b29273a
Tsutomu Yoshida <tsuntsunkobi@icloud.com>
2021-08-21 18:55:27 +0900

    initiated SwiftAP242PDMkit package

--- Package.swift
+++ Package.swift
@@ -4,25 +4,32 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftAP242PDMkit",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "SwiftAP242PDMkit",
-            targets: ["SwiftAP242PDMkit"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SwiftAP242PDMkit",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftAP242PDMkitTests",
-            dependencies: ["SwiftAP242PDMkit"]),
-    ]
+	name: "SwiftAP242PDMkit",
+	platforms: [
+		.macOS(.v10_15),
+	],
+	products: [
+		// Products define the executables and libraries a package produces, and make them visible to other packages.
+		.library(
+			name: "SwiftAP242PDMkit",
+			targets: ["SwiftAP242PDMkit"]),
+	],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		// .package(url: /* package url */, from: "1.0.0"),
+		.package(name: "SwiftSDAIcore", path: "/Users/home/Dropbox/STEP/STEPcode/SwiftSDAIcore/"),
+		.package(name: "SwiftSDAIap242", path: "/Users/home/Dropbox/STEP/STEPcode/SwiftSDAIap242/"),
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages this package depends on.
+		.target(
+			name: "SwiftAP242PDMkit",
+			dependencies: []),
+		.testTarget(
+			name: "SwiftAP242PDMkitTests",
+			dependencies: ["SwiftAP242PDMkit"]),
+	]
 )
+
+
